7800a52f75f67e471d2fb78737ee7370aa03c9dc
Abdalrahman Abdalqader <salehabod63@gmail.com>
2025-05-19 23:19:07 +0300

    add contaner1 content div books name

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
     "images/image/book_14.png",
     "images/image/book_15.png"
   ];  } );
+
+
+  function createBookCard(imageSrc) {
+    return `
+      <div class="book-card">
+        <img src="${imageSrc}" alt="Book">
+        <div class="Featured-Books"><span>Featured</span> <span>Books</span></div>
+        <div><span class="jone-Deo">john Deo</span></div>
+        <div class="Romance">
+          <span>Thiller</span>, <span>Horror</span>, <span>Romance</span>
+        </div>
+        <div class="cost-Book"><span>$25.50</span></div>
+        <div class="cost-past"><span><del>$28.60</del></span></div>
+        <input class="button-learn3" type="button" value="Learn More">
+      </div>
+    `;
+  }
+
+
+  const container1 = document.getElementById("Books-name");
+  if (container1) {
+    container1.innerHTML = books.map(createBookCard).join("");
+  }
